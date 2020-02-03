b5ca2bcec6e2a17e1283383e8db472640009104a
Aniket Raj <aniketraj.go@gmail.com>
2020-02-03 19:12:02 +0530

    linear search method-1

---
+++ coding blocks/Arrays/linear search(a).cpp
@@ -0,0 +1,19 @@
+// linear search
+#include<iostream>
+using namespace std;
+int main(){
+int arr[10]={5,22,2,6,3,2,7,4,9};
+int k,p=0;
+int n=sizeof(arr)/sizeof(int);
+cout<<"Enter number to be search"<<endl;
+cin>>k;
+for(int i=0;i<10;i++){
+
+    if(arr[i]==k){
+            cout<<k<<" is present in the array at location "<<i+1<<endl;
+    p++;
+    }
+}
+if(p==0){ cout<<k<<" is Not present in the array"<<endl;}
+
+}
