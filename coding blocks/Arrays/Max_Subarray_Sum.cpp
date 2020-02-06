4bab2ed40207070bf88031187884aa920a641a45
Aniket Raj <aniketraj.go@gmail.com>
2020-02-06 17:52:38 +0530

    Maximum Sum of sub-arrays using cumulative_sum

--- coding blocks/Arrays/Max_Subarray_Sum.cpp
+++ coding blocks/Arrays/Max_Subarray_Sum.cpp
@@ -1,32 +1,35 @@
+// Maximum Sum of sub-arrays using cumulative_sum
 #include<iostream>
 #include<string.h>
 using namespace std;
 int main(){
-int arr[]={1,2,3,-4};
+int arr[]={-4,1,3,-2,6,2,-8,-9,4};
 int n=sizeof(arr)/sizeof(int);
-int cum_sum[n]={0},max_sum=0;
-cum_sum[0]=0;
+cout<<"You Entered : " ;
+for(int i=0;i<n;i++){
+cout<<arr[i]<<" ";}
+cout<<endl;
+int cumulative_sum[n]={0},max_sum=0,sum;
+cumulative_sum[0]=arr[0];
+cout<<"Cumulative Sum of Elements: ";
 for(int i=1;i<=n;i++){
-  cum_sum[i]=cum_sum[i]+arr[i-1];
+  cumulative_sum[i]=cumulative_sum[i-1]+arr[i];
 }
-for(int i=0;i<=n;i++)
-cout<<cum_sum[i]<<endl;
-//int left,right;
-//for(int i=0;i<n;i++){
-//    for(int j=i;j<n;j++){
-//        for(int k=i;k<=j;k++){
-//        sum=sum+arr[k];
-//       // cout<<arr[k];
-//        }
-//       // cout<<sum;
-//        if(sum>max_sum){
-//        max_sum=sum;
-//        left=i; right=j;
-//        }
-//         sum=0;
-//    }
-//}
-//cout<<"maximum sum="<<max_sum<<endl;
-//for(int i=left;i<=right;i++)
-//    cout<<arr[i]<<",";
+for(int i=0;i<n;i++){
+cout<<cumulative_sum[i]<<" ";}
+cout<<endl;
+int left,right;
+for(int i=0;i<n;i++){
+    for(int j=i;j<n;j++){
+      sum=i>0?cumulative_sum[j]-cumulative_sum[i-1]:cumulative_sum[j];
+        if(sum>max_sum){
+        max_sum=sum;
+        left=i; right=j;
+        }
+         sum=0;
+    }
+}
+cout<<"maximum sum="<<max_sum<<endl;
+for(int i=left;i<=right;i++)
+    cout<<arr[i]<<",";
 }
