f428b974231f118602f6cb407c52ec0ad48e0ffa
Aniket Raj <aniketraj.go@gmail.com>
2020-02-07 23:16:55 +0530

    Create rain_watar_trapping.cpp

---
+++ coding blocks/Arrays/rain_watar_trapping.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int c,n;
+    cin>>c>>n;
+    int a[n]={0,2,1,3,0,1,2,1,2,1};
+    while(c--){
+        for(int i=0;i<n;i++){
+        if(a[i]>a)
+            
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
