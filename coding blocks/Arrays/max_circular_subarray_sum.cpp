5696504676aa670aab6de2d81b9d3c24b0f32a46
Aniket Raj <aniketraj.go@gmail.com>
2020-02-10 01:17:05 +0530

    Maximum Circular Sub-array Sum

--- coding blocks/Arrays/max_circular_subarray_sum.cpp
+++ coding blocks/Arrays/max_circular_subarray_sum.cpp
@@ -1,20 +1,38 @@
-// Maximum Circular Subarray Sum
+// Maximum Circular Sub-array Sum
 #include<iostream>
+#include<algorithm>
 using namespace std;
-int kadanes(int *a)
-int arr[]={1,-9,-2,4,-8,7,-9,2};
-int n=sizeof(arr)/sizeof(int);
-int current_sum=0,max_sum=0,sum=0;
+int kadanes(int *a,int n){
+int cs=0,max_sum=0;   // cs=>current sum
 for(int i=0;i<n;i++){
-    if(current_sum+arr[i]>=0){
-        current_sum=current_sum+arr[i];}
-        else current_sum=0;
-            max_sum=max(current_sum,max_sum);
-    }
+    if(cs+a[i]>=0)
+        cs+=a[i];
+    else cs=0;
+    max_sum=max(max_sum,cs);
+}
+return max_sum;
+}
+int cumulative_sum(int *a,int n){
+    int c_s=0;
+for(int i=0;i<n;i++){
+    c_s+=a[i];
+}
+return c_s;
+}
+int sign_reverse(int k){return -k;}
 
-cout<<"Maximum Sum: "<<max_sum;
+int max_circular_sum(int *a,int n){
+int non_cir_sum=kadanes(a,n);
+int cuml_sum=cumulative_sum(a,n);
+transform(a,a+n,a,sign_reverse);
+int cir_sum=cuml_sum-(-kadanes(a,n));
+return non_cir_sum>=cir_sum?non_cir_sum:cir_sum;
 }
 
-
 int main(){
+    int a[]={10,-3,-4,7,6,5,-4,-1};
+    int n=sizeof(a)/sizeof(int);
+    cout<<"Max Sum : "<<max_circular_sum(a,n);
+
+}
 
