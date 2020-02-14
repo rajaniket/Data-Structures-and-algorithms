5f7130b90e72b0e8774f7ae48f65d8428bb07ad3
Aniket Raj <aniketraj.go@gmail.com>
2020-02-14 23:45:46 +0530

    Read a matrix and Print its wave

---
+++ coding blocks/Arrays 2.0/Print_wave.cpp
@@ -0,0 +1,36 @@
+// Read a matrix and Print its wave
+#include<iostream>
+#include<cstring>
+using namespace std;
+void read_matrix(int a[][10],int r,int c){
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>a[i][j];
+        }
+    }
+}
+void print_matrix(int a[][10],int r,int c){
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+void wave_print(int a[][10],int r,int c){
+    for(int i=0;i<c;i++){
+        for(int j=0;j<r;j++){
+           if(i%2==0)cout<<a[j][i]<<" ";
+           else cout<<a[r-j-1][i]<<" ";
+        }
+    }}
+
+int main(){
+int a[10][10];
+int r,c;
+cin>>r>>c;
+read_matrix(a,r,c);
+print_matrix(a,r,c);
+cout<<endl<<"........................Wave print.........................."<<endl<<endl;
+wave_print(a,r,c);
+}
