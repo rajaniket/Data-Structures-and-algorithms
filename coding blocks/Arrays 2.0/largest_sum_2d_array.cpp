eb80509dc32af1401f9438229235df72980ecb66
Aniket Raj <aniketraj.go@gmail.com>
2020-02-19 23:39:03 +0530

    Create largest_sum_2d_array.cpp

---
+++ coding blocks/Arrays 2.0/largest_sum_2d_array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+void read_matrix(int a[][10],int r,int c){
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>a[i][j];
+        }
+    }
+}
+void print_matrix(int a[][10],int r,int c){
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
