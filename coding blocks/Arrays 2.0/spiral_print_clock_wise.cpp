823dab773c9d078f9ac60843608beb66ce6f4d5c
Aniket Raj <aniketraj.go@gmail.com>
2020-02-21 23:43:03 +0530

    Spiral print of a matrix clock-wise

--- coding blocks/Arrays 2.0/spiral_print_clock_wise.cpp
+++ coding blocks/Arrays 2.0/spiral_print_clock_wise.cpp
@@ -1,5 +1,4 @@
-// Spiral print of a matrix
-// Method-2
+// Spiral print of a matrix clock-wise
 // input => 4 4 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35
 // input=> 6 3 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34
 #include<iostream>
