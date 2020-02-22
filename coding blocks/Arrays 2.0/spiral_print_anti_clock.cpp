815296fdd1b3d7abb25fcc16c678856ee6e9faa4
Aniket Raj <aniketraj.go@gmail.com>
2020-02-22 19:03:48 +0530

    Print Spiral elements of a matrix anti-clock wise

--- coding blocks/Arrays 2.0/spiral_print_anti_clock.cpp
+++ coding blocks/Arrays 2.0/spiral_print_anti_clock.cpp
@@ -1,6 +1,5 @@
-// Print Spiral elements of a matrix anticlock wise
-// input => 4 4 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
-
+// Print Spiral elements of a matrix (anti clock wise)
+// input => 4 4 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 48 49 50
 #include<iostream>
 #include<cstring>
 using namespace std;
@@ -24,26 +23,28 @@ int end_co1=(c-1);
 int end_row=(r-1),start_col=0,start_row=0;
 while(start_col<=end_co1&&start_row<=end_row){
 //Starting row
-if(start_row>end_row){
+
 for(int i=start_row;i<=end_row;i++){
     cout<<a[i][start_col]<<" ";
-}}
+}
 start_col++;
 //Starting column
-if(start_col<end_co1){
+
 for(int j=start_col;j<=end_co1;j++){
     cout<<a[end_row][j]<<" ";
-}}
+}
 end_row--;
 //Ending Row
+if(start_col<=end_co1){
 for(int i=end_row;i>=start_row;i--) {
     cout<<a[i][end_co1]<<" ";
-}
+}}
 end_co1--;
 //Ending Column
+if(start_row<=end_row){
 for(int j=end_co1;j>=start_col;j--){
     cout<<a[start_row][j]<<" ";
-}
+}}
 start_row++;
 }
 
@@ -53,9 +54,10 @@ int a[10][10];
 int r,c;
 cin>>r>>c;
 read_matrix(a,r,c);
+cout<<endl;
 print_matrix(a,r,c);
 //search_element(18,a,r,c);
+cout<<endl;
+
 spiral_print(a,r,c);
 }
-
-
