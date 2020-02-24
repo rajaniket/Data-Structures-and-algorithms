abb4e265470c6808633fdda1e1cbf901e9d0fc69
Aniket Raj <aniketraj.go@gmail.com>
2020-02-24 23:24:21 +0530

    sorting string class array

---
+++ coding blocks/Arrays 2.0/sort_strings.cpp
@@ -0,0 +1,29 @@
+//sorting string class array
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+bool rev_comp(string a,string b){
+    cout<<a<<"  "<<b<<endl;
+    return a>b;
+}
+int main(){
+string arr[]={"potato","zebra","banana","apple","mango","radish","sweet_potato"};
+int p=sizeof(arr)/sizeof(string);
+cout<<"Total Number of String: "<<p<<endl;
+
+for(int i=0;i<p;i++)
+    cout<<arr[i]<<endl;
+
+cout<<"Sorting lexicographically (forward alphabetically )"<<endl<<endl;
+sort(arr,arr+(p-1));
+for(int i=0;i<p;i++)
+    cout<<arr[i]<<endl;
+cout<<"\nSorting reverse lexicographically (reverse alphabetically )"<<endl<<endl;
+sort(arr,arr+p ,rev_comp);
+
+for(int i=0;i<p;i++)
+    cout<<arr[i]<<endl;
+
+}
+
