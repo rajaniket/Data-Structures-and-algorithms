39486825714a667aaa24d14f569c67defef77704
Aniket Raj <aniketraj.go@gmail.com>
2020-02-26 22:48:01 +0530

    My_tokenizer function

--- coding blocks/Arrays 2.0/my_str_tokenizer.cpp
+++ coding blocks/Arrays 2.0/my_str_tokenizer.cpp
@@ -1,32 +1,38 @@
+//Mytokenizer function (like tokenizer function which is predefined)
 #include<iostream>
 #include<algorithm>
 #include<cstring>
 using namespace std;
-char *mystrtok(char a[],char delm){
+char *mystrtok(char arr[],char del[]){
 static char *input=NULL;
-if(a!=NULL)
-    input==a;
+    if(arr!=NULL){
+      input=arr;
+    }
+int k=0;
 char *output=new char[strlen(input)+1];
 int i;
 for(i=0;input[i]!='\0';i++){
-if(input[i]!=delm){
-        output[i]=input[i];
+        for(int k=0;k<strlen(del);k++)
+        if(input[i]!=del[k])
+    output[i]=input[i];
+else{
+    output[i]='\0';
+input=input+i+1;
+return output;
 }
-else {
-    output[i+1]='\0';
-    input=input+i+1;
-    return output;
-
-}}
-
 }
+output[i]='\0';
+input=NULL;
+return output;
+}
+
 int main(){
-char a[]={"Hi, My name is Aniket, My roll number is 31"};
+char a[]={"Hi,My name is Aniket, My roll number is 31"};
 char *p;
-p=mystrtok(a,' ');
+p=mystrtok(a,",");
 while(p!=NULL){
 cout<<p<<endl;
-p=mystrtok(NULL,' ');
+p=mystrtok(NULL,",");
 }
 }
 
