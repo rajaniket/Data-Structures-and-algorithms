261d663d22caeae26e2515020e140018ff2b3298
Aniket Raj <aniketraj.go@gmail.com>
2020-03-07 23:51:16 +0530

    Update Sub_matrix_sum(b).cpp

--- coding blocks/Problem solving on 2d array/Sub_matrix_sum(b).cpp
+++ coding blocks/Problem solving on 2d array/Sub_matrix_sum(b).cpp
@@ -13,19 +13,38 @@ void printmatrix(int a[][10],int r,int c){
 for(int i=0;i<r;i++){
         cout<<endl;
     for(int j=0;j<c;j++)
-    cout<<a[i][j];
+    cout<<a[i][j]<<" ";
 }
 cout<<endl;
 }
-
 void generate_sum(int a[][10],int r,int c){
 int sum=0;
+int cum_sum=0;
+int temp[r][10]={0};
+for(int i=0;i<r;i++){
+        cum_sum=0;
+for(int j=0;j<c;j++){
+        cum_sum+=a[i][j];
+    if(i==0)
+    temp[i][j]=cum_sum;
+    else
+    temp[i][j]=cum_sum+temp[i-1][j];
+}}
+cout<<"Cumulative sum "<<endl;
+printmatrix(temp,r,c);
+
 for(int i_left=0;i_left<r;i_left++)
     for(int i_right=0;i_right<c;i_right++)
        for(int j_left=i_left;j_left<r;j_left++)
-          for(int j_right=i_right;j_right<c;j_right++)
+          for(int j_right=i_right;j_right<c;j_right++){
+            if(i_left==0&&i_right==0)
+                sum+=temp[j_left][j_right];
+            else sum+=temp[j_left][j_right]-temp[j_left][i_right]-temp[j_]
             
-              }
+          }
+             
+              
+
 }
 int main(){
     int r,c;
@@ -35,3 +54,5 @@ int main(){
   printmatrix(arr,r,c);
   generate_sum(arr,r,c);
 }
+
+
