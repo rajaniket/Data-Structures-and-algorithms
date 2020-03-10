159135dcde18316fee24509bac2d643ec62c3b8a
Aniket Raj <aniketraj.go@gmail.com>
2020-03-10 23:54:22 +0530

    Update Sub_matrix_sum(c).cpp

--- coding blocks/Problem solving on 2d array/Sub_matrix_sum(c).cpp
+++ coding blocks/Problem solving on 2d array/Sub_matrix_sum(c).cpp
@@ -1,8 +1,9 @@
 //Sum of all sub-matrix from a given matrix
-//Method-3
-
+//Method-3 -> O(n^2)
+//
 #include<iostream>
 #include<string.h>
+
 using namespace std;
 void getmatrix(int a[][10],int r,int c){
 for(int i=0;i<r;i++)
@@ -13,16 +14,18 @@ void printmatrix(int a[][10],int r,int c){
 for(int i=0;i<r;i++){
         cout<<endl;
     for(int j=0;j<c;j++)
-    cout<<a[i][j];
+    cout<<a[i][j]<<" ";
 }
 cout<<endl;
 }
-
 void generate_sum(int a[][10],int r,int c){
 int sum=0;
 for(int i=0;i<r;i++)
     for(int j=0;j<c;j++)
- 
+      sum+=a[i][j]*(j+1-0)*(c-j)*(i+1-0)*(r-i); // zero indicates left minimum column or top most row ,c->right most column,r->down most column
+
+    cout<<"sum="<<sum;
+}
 int main(){
     int r,c;
     cin>>r>>c;
@@ -31,3 +34,5 @@ int main(){
   printmatrix(arr,r,c);
   generate_sum(arr,r,c);
 }
+
+
