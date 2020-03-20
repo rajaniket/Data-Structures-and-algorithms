4e50b89d279817ccd1be867955503b8a5c8a1a6a
Aniket Raj <aniketraj.go@gmail.com>
2020-03-20 23:40:21 +0530

    Create palindromic_string.cpp

---
+++ coding blocks/Arrays/palindromic_string.cpp
@@ -0,0 +1,19 @@
+#include"iostream"
+#include"cstring"
+using namespace std;
+void palindrom(const char *a)
+{
+    int l=strlen(a);
+    char b[l];
+    for(int i=0;i<a;i++)
+        b[i]==a
+    
+    
+}
+int main(){
+char *a="raar"
+palindrom(a);
+
+
+
+}
