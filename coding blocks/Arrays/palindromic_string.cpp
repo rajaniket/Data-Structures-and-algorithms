a908ec3b0f9bf9f4a997ee30c3271f19ca37265f
Aniket Raj <aniketraj.go@gmail.com>
2020-03-21 20:02:36 +0530

    Check string is palindromic or not

--- coding blocks/Arrays/palindromic_string.cpp
+++ coding blocks/Arrays/palindromic_string.cpp
@@ -1,24 +1,24 @@
+// Check string is palindromic or not
 #include"iostream"
 #include"cstring"
 using namespace std;
 void palindrom(const char *a)
 {
     int l=strlen(a);
-    char b[l];
-    for(int i=0;i<l;i++)
-        b[i]==a[l-1-i];
-    int i;
-    for(i=0;i<l;i++)
-        if(a[i]!=b[i])
-        break;
-    if(i==l)
-        cout<<"Palindrom"<<endl;
-    else cout<<"Not a Palindrom"<<endl;
+    l--;
+    for(int i=0;i<=l-i;i++){
+            if(a[i]!=a[l-i]){
+                cout<<a<<" is not a Palindrom"<<endl;
+                return;
+            }
+    }
+        cout<<a<<" is a Palindrom"<<endl;
 }
 int main(){
-char *a="raar";
+char *a="123454321";
 palindrom(a);
-
-
-
+char *a1="abcdcba";
+palindrom(a1);
+char *a2="aniket";
+palindrom(a2);
 }
