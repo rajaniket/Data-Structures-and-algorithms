8e5b754b48de84d6460e1d7165051ab13f7cd462
Aniket Raj <aniketraj.go@gmail.com>
2020-03-22 23:38:52 +0530

    Remove consecutive duplicates m-2 ,O(n)

--- coding blocks/Arrays/remove_consecutive_duplicates(b).cpp
+++ coding blocks/Arrays/remove_consecutive_duplicates(b).cpp
@@ -1,21 +1,21 @@
 // remove consecutive duplicates(m-2 ,O(n) )
-// "ccccoooooooodinnnnngggg__bloockksssss"===> coding_blocks
+// "ccoodinggg__bloockksssss"===> coding_blocks
 #include"iostream"
 #include"cstring"
 using namespace std;
 void duplicate_remove(char *a)
 {
     int l=strlen(a);
+   int previous=0;
     for(int i=1;i<l;i++){
-        if(a[i-1]==a[i]){
-    //    cout<<","<<a[i-1]<<","<<a[i];
-         a[i-1]=a[i];
-         i--;
-         l--;
-        }
-}}
+        if(a[previous]!=a[i]){
+        previous++;
+        a[previous]=a[i];
+        }}
+    a[previous+1]='\0';
+}
 int main(){
-char a[]="coodinngbloockksssss";
+char a[]="c";
 duplicate_remove(a);
 cout<<a;
 }
