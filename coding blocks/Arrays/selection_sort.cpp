f21f14e9c79e7a3b8a0c5082f9138f254f99f5ce
Aniket Raj <aniketraj.go@gmail.com>
2020-03-24 23:57:00 +0530

    Update selection_sort.cpp

--- coding blocks/Arrays/selection_sort.cpp
+++ coding blocks/Arrays/selection_sort.cpp
@@ -1,8 +1,8 @@
-//Binary search implementation
+//Selection sort implementation
 #include<iostream>
 #include<string.h>
 using namespace std;
-void selection_sort_accending(int *a,int l){
+void selection_sort_ascending(int *a,int l){
     int Min;
 for(int i=0;i<l;i++){
        Min=i;
@@ -13,6 +13,17 @@ for(int i=0;i<l;i++){
     swap(a[i],a[Min]);
 }
 }
+void selection_sort_descending(int *a,int l){
+    int Max;
+for(int i=0;i<l;i++){
+       Max=i;
+    for(int j=i;j<l;j++){
+        if(a[Max]<a[j])
+           Max=j;
+    }
+    swap(a[i],a[Max]);
+}
+}
 void print(int *a,int l){
 for(int i=0;i<l;i++){
     cout<<a[i]<<",";
@@ -21,6 +32,10 @@ cout<<endl;
 }
 int main(){
 int a[7]={10,21,2,13,4,25,16};
-selection_sort_accending(a,7);
+selection_sort_ascending(a,7);
 print(a,7);
+int b[7]={10,21,2,13,4,25,16};
+selection_sort_descending(a,7);
+print(b,7);
+
 }
