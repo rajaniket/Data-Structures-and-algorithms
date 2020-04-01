207b3f5b5431d6af2f0f6cd3d1c1be5a70f2e9e3
Aniket Raj <aniketraj.go@gmail.com>
2020-04-01 23:50:53 +0530

    Max Sum Submatrix In Row Col Wise Sorted Matrix

--- coding blocks/Arrays 2.0/Max Sum Submatrix In Row Col Wise Sorted Matrix .cpp
+++ coding blocks/Arrays 2.0/Max Sum Submatrix In Row Col Wise Sorted Matrix .cpp
@@ -1,9 +1,9 @@
-//1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+//Max Sum Submatrix In Row Col Wise Sorted Matrix
 #include<iostream>
 using namespace std;
 void max_sum(int a[][10],int m,int n){
     int b[m][n]={0};
-for(int i=m-1;i>=0;i--){
+for(int i=m-1;i>=0;i--){ //subfix => prefix in reverseway
         int sum=0;
     for(int j=n-1;j>=0;j--){
     b[i][j]=sum+a[i][j];
@@ -24,7 +24,9 @@ cout<<Max<<endl;
 int main(){
 int m=3,n=3;
 int a[m][10];
-
+for(int i=0;i<m;i++)
+for(int j=0;j<n;j++){
+   cin>>a[i][j];
 }
 max_sum(a,m,n);
 }
