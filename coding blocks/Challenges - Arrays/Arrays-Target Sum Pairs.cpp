4de9cf32bfe530182af2ce7df11464d7c1470016
Aniket Raj <aniketraj.go@gmail.com>
2020-04-02 23:59:12 +0530

    all pairs of numbrs which sum to target

---
+++ coding blocks/Challenges - Arrays/Arrays-Target Sum Pairs.cpp
@@ -0,0 +1,55 @@
+/*
+Take as input N, the size of array. Take N more inputs and store that in an array. Take as input “target”, a number. Write a function which prints all pairs of numbers which sum to target.
+
+Input Format
+The first line contains input N. Next N lines contains the elements of array and (N+1)th line contains target number.
+
+Constraints
+Length of the arrays should be between 1 and 1000.
+
+Output Format
+Print all the pairs of numbers which sum to target. Print each pair in increasing order.
+
+Sample Input
+5
+1
+3
+4
+2
+5
+5
+Sample Output
+1 and 4
+2 and 3
+Explanation
+Find any pair of elements in the array which has sum equal to target element and print them.
+*/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void pair(int *a,int n,int key){
+sort(a,a+n);
+	int i=0,j=n-1;
+	while(i<j){
+ int e=a[i]+a[j];
+    if(e==key){
+    cout<<a[i]<<" and "<<a[j]<<endl;
+    i++;
+    j--;
+    }
+    else if(e>a[j])
+        j--;
+    else i++;
+	}
+}
+int main() {
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	cin>>a[i];
+	int key;
+	cin>>key;
+	pair(a,n,key)
+	}
+
