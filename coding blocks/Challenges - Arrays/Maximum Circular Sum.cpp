916598f9f46cd4a4b64a59aa9c506d15809412b8
Aniket Raj <aniketraj.go@gmail.com>
2020-04-04 23:50:28 +0530

    Update Maximum Circular Sum.cpp

--- coding blocks/Challenges - Arrays/Maximum Circular Sum.cpp
+++ coding blocks/Challenges - Arrays/Maximum Circular Sum.cpp
@@ -1,4 +1,27 @@
+/*
+You are provided n numbers (both +ve and -ve). Numbers are arranged in a circular form. You need to find the maximum sum of consecutive numbers.
 
+Input Format
+First line contains integer t which is number of test case.
+For each test case, it contains an integer n which is the size of array and next line contains n space separated integers denoting the elements of the array.
+
+Constraints
+1<=t<=100
+1<=n<=1000
+|Ai| <= 10000
+
+Output Format
+Print the maximum circular sum for each testcase in a new line.
+
+Sample Input
+1
+7
+8 -8 9 -9 10 -11 12
+Sample Output
+22
+Explanation
+Maximum Circular Sum = 22 (12 + 8 - 8 + 9 - 9 + 10)
+*/
 
 #include<iostream>
 #include<algorithm>
