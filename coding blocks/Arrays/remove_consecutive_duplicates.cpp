aeae051e7888a8c8c423b2097e4c27246e60d62f
Aniket Raj <aniketraj.go@gmail.com>
2020-04-14 23:55:52 +0530

    remove consecutive duplicates M-1 O(n^2)

--- coding blocks/Arrays/remove_consecutive_duplicates.cpp
+++ coding blocks/Arrays/remove_consecutive_duplicates.cpp
@@ -1,5 +1,5 @@
 // remove consecutive duplicates(m-1 ,O(n^2) )
-// "ccccoooooooodinnnnngggg__bloockksssss"===> coding_blocks
+// "ccccooodinnnnngggg__bloockksssss"===> coding_blocks
 #include"iostream"
 #include"cstring"
 using namespace std;
