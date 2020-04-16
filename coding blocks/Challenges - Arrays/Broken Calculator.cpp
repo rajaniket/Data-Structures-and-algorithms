0e0045f4a0221e4de5b8f23c1946f0abde5e184d
Aniket Raj <aniketraj.go@gmail.com>
2020-04-16 23:56:08 +0530

    Create Broken Calculator.cpp

---
+++ coding blocks/Challenges - Arrays/Broken Calculator.cpp
@@ -0,0 +1,19 @@
+/*
+Andrew was attempting a mathematics test where he needed to solve problems with factorials. One such problem had an answer which equaled 100! ,He wondered what would this number look like. He tried to calculate 100! On his scientific calculator but failed to get a correct answer. Can you write a code to help Andrew calculate factorials of such large numbers?
+
+Input Format
+a single lined integer N
+
+Constraints
+1 < = N < = 500
+
+Output Format
+Print the factorial of N
+
+Sample Input
+5
+Sample Output
+120
+Explanation
+for factorial of 5 we have 5 x 4 x 3 x 2 x 1 = 120
+*/
