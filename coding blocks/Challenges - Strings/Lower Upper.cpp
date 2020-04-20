bf3edb61e1fd4711008c8997e5980113c0dd6767
Aniket Raj <aniketraj.go@gmail.com>
2020-04-20 23:25:12 +0530

    Detect Uppercase and lowercase alphabet

--- coding blocks/Challenges - Strings/Lower Upper.cpp
+++ coding blocks/Challenges - Strings/Lower Upper.cpp
@@ -1,3 +1,23 @@
+/*
+Print "lowercase" if user enters a character between 'a-z' , Print "UPPERCASE" is character lies between 'A-Z' and print "Invalid" for all other characters like $,.^# etc.
+
+Input Format
+Single Character .
+
+Constraints
+-
+
+Output Format
+lowercase UPPERCASE Invalid
+
+Sample Input
+$
+Sample Output
+Invalid
+Explanation
+-
+*/
+
 #include<iostream>
 #include<string.h>
 using namespace std;
