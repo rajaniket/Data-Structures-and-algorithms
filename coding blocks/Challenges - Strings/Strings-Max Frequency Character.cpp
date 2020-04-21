fea4c9b2294026420232454ae290e031f641646f
Aniket Raj <aniketraj.go@gmail.com>
2020-04-21 23:53:20 +0530

    character with maximum frequency in a string

--- coding blocks/Challenges - Strings/Strings-Max Frequency Character.cpp
+++ coding blocks/Challenges - Strings/Strings-Max Frequency Character.cpp
@@ -1,3 +1,24 @@
+/*
+Take as input S, a string. Write a function that returns the character with maximum frequency. Print the value returned.
+
+Input Format
+String
+
+Constraints
+A string of length between 1 to 1000.
+
+Output Format
+Character
+
+Sample Input
+aaabacb
+Sample Output
+a
+Explanation
+For the given input string, a appear 4 times. Hence, it is the most frequent character.
+
+*/
+
 #include<iostream>
 #include<string.h>
 #include<algorithm>
