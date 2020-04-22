0b6be0e66189f6f3ccd591628ee97b41892979ea
Aniket Raj <aniketraj.go@gmail.com>
2020-04-22 23:55:19 +0530

    check palindrome

--- coding blocks/Challenges - Strings/Is Palindrome.cpp
+++ coding blocks/Challenges - Strings/Is Palindrome.cpp
@@ -1,7 +1,24 @@
 /*
+Take as input N, a number. Take N more inputs and store that in an array. Write a recursive function which tests if the array is a palindrome and returns a Boolean value. Print the value returned.
 
+Input Format
+Enter a number N , add N more numbers
+
+Constraints
+None
+
+Output Format
+Display the Boolean result
+
+Sample Input
+4
+1
+2
+2
+1
+Sample Output
+true
 */
-
 #include<iostream>
 #include<string.h>
 #include<algorithm>
