9a80f8052ca195c3269c2f5105b67266c80586d4
Aniket Raj <aniketraj.go@gmail.com>
2020-05-11 23:52:54 +0530

    Create String Compression.cpp

---
+++ coding blocks/Challenges - Strings/String Compression.cpp
@@ -0,0 +1,24 @@
+/*
+Take as input S, a string. Write a function that does basic string compression. Print the value returned. E.g. for input “aaabbccds” print out a3b2c2d1s1.
+
+Input Format
+A single String S
+
+Constraints
+1 < = length of String < = 1000
+
+Output Format
+The compressed String.
+
+Sample Input
+aaabbccds
+Sample Output
+a3b2c2d1s1
+Explanation
+In the given sample test case 'a' is repeated 3 times consecutively, 'b' is repeated twice, 'c' is repeated twice and 'd and 's' occurred only once.
+*/
+#include<iostream>
+using namespace std;
+int main(){
+
+}
