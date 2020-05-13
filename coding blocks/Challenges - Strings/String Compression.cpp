489898bfdb7adc663996cb91fd33f53520b03463
Aniket Raj <aniketraj.go@gmail.com>
2020-05-13 23:53:53 +0530

    aaabbccds --->a3b2c2d1s1

--- coding blocks/Challenges - Strings/String Compression.cpp
+++ coding blocks/Challenges - Strings/String Compression.cpp
@@ -14,11 +14,28 @@ Sample Input
 aaabbccds
 Sample Output
 a3b2c2d1s1
+
 Explanation
 In the given sample test case 'a' is repeated 3 times consecutively, 'b' is repeated twice, 'c' is repeated twice and 'd and 's' occurred only once.
 */
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
 
+int main() {
+string a;
+cin>>a;
+int left=0,right=0;
+int len=a.length();
+while(right<len){
+if(a[right+1]==a[left]){
+right++;
 }
+else{
+cout<<a[left];
+cout<<right-left+1;
+right++;
+left=right;
+}
+}
+}
+
