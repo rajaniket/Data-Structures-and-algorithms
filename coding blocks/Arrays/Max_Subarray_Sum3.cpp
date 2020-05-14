9ecd0635727cb370e553f395519c9c384ed58582
Aniket Raj <aniketraj.go@gmail.com>
2020-05-14 22:35:21 +0530

    Kadane's Algorithm

--- coding blocks/Arrays/Max_Subarray_Sum3.cpp
+++ coding blocks/Arrays/Max_Subarray_Sum3.cpp
@@ -2,6 +2,9 @@
 // 1 -2 4 2
 //sum=>1 0 4 6
 //max_sum= 1->4->6
+/*
+
+// this program will give zero in case arr[]={-2,-1,-3}
 #include<iostream>
 using namespace std;
 int main(){
@@ -17,3 +20,22 @@ for(int i=0;i<n;i++){
 
 cout<<"Maximum Sum: "<<max_sum;
 }
+
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+int arr[]={-2};
+int n=sizeof(arr)/sizeof(int);
+int current_sum=0,max_sum=INT_MIN,sum=0;
+for(int i=0;i<n;i++){
+        current_sum=current_sum+arr[i];
+        max_sum=max(current_sum,max_sum);
+        if(current_sum<0)
+            current_sum=0;
+    }
+
+cout<<"Maximum Sum: "<<max_sum;
+}
+
