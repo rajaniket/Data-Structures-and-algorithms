4b2ab866a48512fe2b6b02d11afa63e275014912
Aniket Raj <aniketraj.go@gmail.com>
2020-05-17 23:56:13 +0530

    Update Broken Calculator.cpp

--- coding blocks/Challenges - Arrays/Broken Calculator.cpp
+++ coding blocks/Challenges - Arrays/Broken Calculator.cpp
@@ -17,3 +17,34 @@ Sample Output
 Explanation
 for factorial of 5 we have 5 x 4 x 3 x 2 x 1 = 120
 */
+#include<iostream>
+#include<vector>
+#include<math.h>
+using namespace std;
+int multiply(int n,arr *a,int Count){
+int k,carry=0;
+for(int i=0;i<Count;i++){
+    k=a[i]*n+carry;
+    a[]
+    
+}
+
+
+
+return Count;
+}
+int main(){
+vector<int> a;
+int n;
+cin>>n;
+a[0]='1';
+for(int i=1;i<n;i++){
+
+    
+}
+
+
+
+
+
+}
