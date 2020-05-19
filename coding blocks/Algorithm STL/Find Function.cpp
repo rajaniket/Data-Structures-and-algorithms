66686a1fd4c9aab9e795c591e7cc470cdcd00f53
Aniket Raj <aniketraj.go@gmail.com>
2020-05-19 23:56:52 +0530

    Find function

--- coding blocks/Algorithm STL/Find Function.cpp
+++ coding blocks/Algorithm STL/Find Function.cpp
@@ -1,4 +1,4 @@
-//
+// Find function in stl
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -6,8 +6,8 @@ int main(){
 int arr[]={5,40,28,11,33};
 int n=sizeof(arr)/sizeof(int);
 int key=11;
-auto adrs=find(arr,arr+n,key);
-int index=adrs-arr;
+auto adrs=find(arr,arr+n,key); // you can use int* instead of auto 
+int index=adrs-arr; // subtracting base address to find out its index
 if(index==n)
     cout<<"Number Not Found"<<endl;
 else cout<<"Number present at index "<<index<<endl;
