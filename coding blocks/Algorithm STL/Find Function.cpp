58f7bb92b93ca859b915bfceb40de35efdb0e6c5
Aniket Raj <aniketraj.go@gmail.com>
2020-05-22 23:47:22 +0530

    Update Find Function.cpp

--- coding blocks/Algorithm STL/Find Function.cpp
+++ coding blocks/Algorithm STL/Find Function.cpp
@@ -12,3 +12,21 @@ if(index==n)
     cout<<"Number Not Found"<<endl;
 else cout<<"Number present at index "<<index<<endl;
 }
+
+// in case of vector
+/*
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+vector<int> arr={5,40,28,11,33};
+int n=arr.size();
+int key=28;
+auto adrs=find(arr.begin(),arr.end(),key);
+int index=adrs-arr.begin();
+if(index==n)
+    cout<<"Number Not Found"<<endl;
+else cout<<"Number present at index "<<index<<endl;
+}
+*/
+
