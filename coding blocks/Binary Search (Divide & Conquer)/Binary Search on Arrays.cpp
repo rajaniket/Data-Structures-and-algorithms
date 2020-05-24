7de74996757e6581cc0216fd5a5360e34e905046
Aniket Raj <aniketraj.go@gmail.com>
2020-05-24 23:50:45 +0530

    Binary search implementation

--- coding blocks/Binary Search (Divide & Conquer)/Binary Search on Arrays.cpp
+++ coding blocks/Binary Search (Divide & Conquer)/Binary Search on Arrays.cpp
@@ -1,3 +1,7 @@
+//Binary search implementation 
+#include<iostream>
+#include<string.h>
+using namespace std;
 void Binary_search(int key,int *a,int l){
 int Start=0,End=l-1,Middle;
 while(Start<=End){
@@ -12,4 +16,10 @@ else if(a[Middle]>key)
 else Start=Middle+1;
 }
 cout<<"Not found"<<endl;
+}
+int main(){
+int a[7]={10,11,12,13,14,15,16};
+int b[6]={10,11,12,13,14,15};
+Binary_search(15,a,7); //(Key,array,length)
+Binary_search(123,b,6);
 }
