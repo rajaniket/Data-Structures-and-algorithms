1bc0844652a5b552673d85a42627004efe09fd87
Aniket Raj <aniketraj.go@gmail.com>
2020-05-25 23:51:38 +0530

    Add files via upload

---
+++ leetcode/Two Pointer/square of a sorted array.cpp
@@ -0,0 +1,18 @@
+/*
+Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Example 2:
+
+Input: [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+
+Note:
+
+1 <= A.length <= 10000
+-10000 <= A[i] <= 10000
+A is sorted in non-decreasing order.
+
+*/
