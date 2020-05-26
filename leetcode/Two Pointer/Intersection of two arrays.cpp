c37afb7ea8b111f0d8a41a9cb7e02494f11c0a07
Aniket Raj <aniketraj.go@gmail.com>
2020-05-26 23:27:49 +0530

    Update Intersection of two arrays.cpp

--- leetcode/Two Pointer/Intersection of two arrays.cpp
+++ leetcode/Two Pointer/Intersection of two arrays.cpp
@@ -8,9 +8,65 @@ Output: [2]
 Example 2:
 
 Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-Output: [9,4]
+Output: [9,4] or [4,9]
+Note:
+
+Input: nums1 = [4,9,5,8], nums2 = [9,4,9,8,4]
+Output: [4,8,9]
 Note:
 
 Each element in the result must be unique.
 The result can be in any order.
 */
+
+#include<iostream>
+#include<string.h>
+#include"algorithm"
+#include"vector"
+#include"set"
+using namespace std;
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size();
+        int n2=nums2.size();
+        // 4 5 9       4 4 8 9 9
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        set<int> store_result;
+
+        int i=0;
+        int j=0;
+
+        while(i<n1 && j<n2){
+
+            if(nums1[i]==nums2[j]){
+                store_result.insert(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else if(nums1[i]<nums2[j])
+            {
+                i++;
+            }
+
+        }
+        vector<int> result(store_result.begin(),store_result.end());
+       // if(result.size()>1){
+       //     result.erase(result.begin()+1,result.end()-1);
+       // }
+        return result;
+        }
+};
+int main(){
+Solution Z;
+vector<int> a={4,9,5,8};
+vector<int> b={9,4,9,8,4};
+vector<int> c=Z.intersection(a,b);
+for(int i:c)
+    cout<<i<<", ";
+
+}
