16764286a081c6b2e9376ac38c9f93bc50ff93ee
Aniket Raj <aniketraj.go@gmail.com>
2020-05-29 23:29:52 +0530

    pair<int,string>

--- coding blocks/Algorithm STL/Pair Class.cpp
+++ coding blocks/Algorithm STL/Pair Class.cpp
@@ -10,18 +10,18 @@ cout<<"p :"<<p.first<<" , "<<p.second<<endl;
 
 // pair<int,string> g(p); // copy of p
 
-pair<int,string> g(200,"Mohan"); // copy of p
+pair<int,string> g(200,"Mohan"); 
 p.swap(g);
 cout<<"p :"<<p.first<<" , "<<p.second<<endl;
 cout<<"g :"<<g.first<<" , "<<g.second<<endl;
 
-// take input from user
+// taking input from user
 int a,b;
 cin>>a>>b;
-pair<int,int> p1=make_pair(a,b);
+pair<int,int> p1=make_pair(a,b);  // make_pair is used to make a pair of two data
 cout<<p1.first<<" , "<<p1.second<<endl;
 
-pair<pair<int,int>,string> car;
+pair<pair<int,int>,string> car;  // at place of this we can use class 
 car.second= "Audi";
 car.first.first=10;
 car.first.second=20;
