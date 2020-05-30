4fc0b59a10f8b79834eea26f92b0a428153b4921
Aniket Raj <aniketraj.go@gmail.com>
2020-05-30 23:52:07 +0530

    Container With Most Water ,2 pointer approach

--- leetcode/array/Container With Most Water.cpp
+++ leetcode/array/Container With Most Water.cpp
@@ -1,17 +1,15 @@
 /*
-Given two arrays, write a function to compute their intersection.
-Example 1:
-Input: nums1 = [1,2,2,1], nums2 = [2,2]
-Output: [2]
-Example 2:
-Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-Output: [9,4] or [4,9]
-Note:
-Input: nums1 = [4,9,5,8], nums2 = [9,4,9,8,4]
-Output: [4,8,9]
-Note:
-Each element in the result must be unique.
-The result can be in any order.
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Note: You may not slant the container and n is at least 2.
+
+The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+ 
+Example:
+
+Input: [1,8,6,2,5,4,8,3,7]
+Output: 49
+
 */
 
 #include<iostream>
@@ -63,6 +61,21 @@ public:
 */
 
 // ********************method-3***************************
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+      int Max=0;
+        int i=0,j=height.size()-1;
+        while(i<j){
+            int h=min(height[i],height[j]);
+            Max=max(Max,(j-i)*h);
+            while(height[i]<=h && i<j){i++;}  // as container size decreases it start depending upon hight so if height is less than container size then skip that element or height
+            while(height[j]<=h && i<j){j--;}
+        }
+        return Max;
+    }
+};
+
 
 int main(){
 vector<int> A={1,8,6,2,5,4,8,3,7};
