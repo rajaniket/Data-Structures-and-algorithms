d4e777253eac334ea2cde7b3d293e1537dfd5faa
Aniket Raj <aniketraj.go@gmail.com>
2020-06-02 23:52:08 +0530

    Create Next Permutation.cpp

---
+++ leetcode/array/Next Permutation.cpp
@@ -0,0 +1,30 @@
+/*
+
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+
+The replacement must be in-place and use only constant extra memory.
+
+Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
+
+*/
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n;
+cin>>n;
+int arr[n+1]={0};  
+for(int i=0;i<n;i++)
+cin>>arr[i];  // 123 132 213 231
+if(n==1)
+return;
+
+int left=0;
+int right=1;
+while()
+    }
