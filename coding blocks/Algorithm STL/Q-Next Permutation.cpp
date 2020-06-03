1423de3ff76899556b75135036796f48ff155bb3
Aniket Raj <aniketraj.go@gmail.com>
2020-06-03 21:04:24 +0530

    Update Q-Next Permutation.cpp

--- coding blocks/Algorithm STL/Q-Next Permutation.cpp
+++ coding blocks/Algorithm STL/Q-Next Permutation.cpp
@@ -33,22 +33,37 @@ Explanation
 Possible permutations for {1,2,3} are {1,2,3} , {1,3,2} , {2,1,3} , {2,3,1}, {3,1,2} and {3,2,1}. {1,3,2} is the immediate next permutation after {1,2,3}.
 For the second testcase , {3,2,1} is the last configuration so we print the first permutation as its next permutation.
 */
-
 #include<iostream>
 #include<algorithm>
 using namespace std;
-void next_permu(int *a){
-int n;
-cin>>n;
-int arr[n+1]={0};  
-for(int i=0;i<n;i++)
-cin>>arr[i];  // 123 132 213 231
-if(n==1)
-return;
-
-int left=0;
-int right=1;
-while()
-
-
+void nextPermutation(vector<int>& nums){
+int n=nums.size();
+int i;
+for(i=n-2;i>=0;i--){
+if(nums[i]<nums[i+1])  // in reverse order, find the first number which is in increasing trend (violated number)
+    break;
 }
+if(i<0)   // if it already the greatest number( if no violation) then i must be -ve , we will reverse that number in order to make it lowest
+{
+    reverse(nums.begin(),nums.end());
+    return;
+}
+// now we will sort the right portion from violeted number 
+sort(nums.begin()+i+1,nums.end());   // reverse(nums.begin()+i+1,nums.end()); it can also be use at the place of sort
+for(int j=i+1;j<n;j++){
+   if(nums[i]<nums[j]){     
+    swap(nums[i],nums[j]);   // swap the violetd number to next greater number 
+    return ;}
+}
+}
+int main()
+{
+     vector<int> arr2={3,2,1};
+   for(int i=0;i<24;i++){
+    nextPermutation(arr2);
+    for(int i:arr2)
+        cout<<i;
+    cout<<endl;
+   }
+}
+
