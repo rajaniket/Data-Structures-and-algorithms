2f35b7eb9f30d20d7dbd28547f6f6a2fa13669a9
Aniket Raj <aniketraj.go@gmail.com>
2020-08-24 20:20:01 +0530

    Update stack implementation using vector.cpp

--- coding blocks/stack/stack implementation using vector.cpp
+++ coding blocks/stack/stack implementation using vector.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 #include<algorithm>
 #include<vector>
@@ -13,6 +14,7 @@ public:
     return v.size()==0;
     }
     void pop(){
+     if(!empty())
     v.pop_back();
     }
     int top(){
@@ -22,6 +24,42 @@ public:
 
 int main(){
 Stack s;
+for(int i=1;i<=5;i++){
+    s.push(i*i);
+}
+while(!s.empty()){
+    cout<<s.top()<<endl;
+    s.pop();
+}
+}
+*/
+// generalize the code (will work for all data type) possible using template , just you have to pass the data type while creating an object.
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+template<typename T>
+class Stack{
+private:
+    vector<T> v;
+public:
+    void push(T data){
+        v.push_back(data);
+    }
+    bool empty(){
+    return v.size()==0;
+    }
+    void pop(){
+    if(!empty())
+    v.pop_back();
+    }
+    T top(){
+     return v[v.size()-1];
+    }
+};
+
+int main(){
+Stack<int>s;
 for(int i=1;i<=5;i++){
     s.push(i*i);
 }
