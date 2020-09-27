75ec4dd8e83b53903e5757154f0b16fa7dba52c5
Aniket Raj <aniketraj.go@gmail.com>
2020-09-27 23:10:07 +0530

    Update stock span.cpp

--- coding blocks/stack/stock span.cpp
+++ coding blocks/stack/stock span.cpp
@@ -25,7 +25,7 @@ Note that (for example) S.next(75) returned 4, because the last 4 prices
 (including today's price of 75) were less than or equal to today's price.
 
 */
-
+// method-1: stack<pair<price,span>
 class StockSpanner {
 public:
     stack<pair<int,int>> s;
@@ -43,3 +43,36 @@ public:
         
     }
 };
+
+
+
+
+// method -2: stack<pair<price,index>
+/*
+#include<iostream>
+#include<stack>
+using namespace std; //7 100 80 60 70 60 75 85
+//5 30 35 40 38 35
+int main(){
+int n;
+cin>>n;
+int price[n];
+for(int i=0;i<n;i++){
+     cin>>price[i];
+}
+stack<pair<int,int>> s;
+s.push(make_pair(price[0],0));
+cout<<1<<" ";
+for(int i=1;i<n;i++){
+    int span=1;
+    while(!s.empty() && price[i]>=s.top().first){
+        s.pop();
+    }
+    span=(s.empty())?i+1:i-s.top().second;
+    cout<<span<<" ";
+    s.push({price[i],i});
+}
+cout<<"END"<<endl;
+
+}
+*/
